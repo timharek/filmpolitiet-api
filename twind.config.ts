2f6e42aa9bf05c3d90f1755bbb1a078c57de253e
Tim Hårek Andreassen <tim@harek.no>
2023-05-31 20:25:54 +0200

    feat: Upgrade twind to v1
    Signed-off-by: Tim Hårek Andreassen <tim@harek.no>

--- twind.config.ts
+++ twind.config.ts
@@ -1,5 +1,9 @@
-import { Options } from "$fresh/plugins/twind.ts";
+import { defineConfig } from "@twind/core@1.1.3";
+import presetTailwind from "@twind/preset-tailwind@1.1.4";
 
 export default {
+  ...defineConfig({
+    presets: [presetTailwind()],
+  }),
   selfURL: import.meta.url,
-} as Options;
+};
