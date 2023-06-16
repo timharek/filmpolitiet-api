3583f0a56b8c6d35346ddfa291ab8b67e8e1a7c8
Tim Hårek Andreassen <tim@harek.no>
2023-06-16 21:10:43 +0200

    chore: Update text on index
    Signed-off-by: Tim Hårek Andreassen <tim@harek.no>

--- routes/index.tsx
+++ routes/index.tsx
@@ -7,9 +7,15 @@ export default function Home() {
         <title>Filmpolitiet API</title>
       </Head>
       <div class="p-4 mx-auto max-w-screen-md">
+        <h1 class="text-4xl font-semibold my-6">
+          Filmpolitiets unofficial API
+        </h1>
         <p class="my-6">
           Welcome to Filmpolitiets unofficial API. You can checkout what's been
           cached <a class="underline" href="/entries">here</a>.
+        </p>
+        <p class="my-6">
+          Currently a work in progress, here be dragons.
         </p>
       </div>
     </>
