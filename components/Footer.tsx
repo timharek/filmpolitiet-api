ecc35edb15c6414dba1adb3c2d7fdf6a13aaca9e
Tim Hårek Andreassen <tim@harek.no>
2023-10-08 15:33:40 +0200

    feat: Simplify footer
    Signed-off-by: Tim Hårek Andreassen <tim@harek.no>

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,15 +2,18 @@ import config from "../deno.json" assert { type: "json" };
 
 export function Footer() {
   return (
-    <footer class="bg-lime-200">
+    <footer class="">
       <div class="max-w-screen-md mx-auto px-4 py-6">
         <div class="flex justify-between items-center gap-4 flex-wrap">
-          <div>
-            Made by{"  "}
-            <a href="https://timharek.no" target="_blank" class="underline">
-              Tim Hårek
-            </a>
-          </div>
+          <a
+            href="https://timharek.no"
+            target="_blank"
+            class="underline"
+            title="Made by Tim Hårek"
+          >
+            <span class="sr-only">Made by Tim Hårek</span>
+            <img src="https://timharek.no/.well-known/logo" width="50" />
+          </a>
           <a href={config.changelog} target="_blank" class="underline">
             v{config.version}
           </a>
